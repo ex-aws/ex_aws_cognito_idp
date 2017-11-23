870a14eba2225a0e697ad8fb6c339ea49e7cd390
Michał Muskała <michal@muskala.eu>
2017-11-23 10:33:47 +0100

    Import from PR

--- mix.exs
+++ mix.exs
@@ -5,24 +5,21 @@ defmodule ExAws.Cognito.MixProject do
     [
       app: :ex_aws_cognito,
       version: "0.1.0",
-      elixir: "~> 1.6-dev",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_aws, "~> 2.0"}
     ]
   end
 end
